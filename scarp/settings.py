109c9e433ccbaf8416e15fc36521cbb652dc86c2
ss94866 <68229944+ss94866@users.noreply.github.com>
2020-10-05 10:40:52 +0530

    Update settings.py

--- scarp/settings.py
+++ scarp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ch%$v%%h_ai79b4p*8_a%!^_eeo^b-x2g$1+d=-c5-w2^$^+c+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['mechanicalgrade.herokuapp.com', '127.0.0.1', 'localhost','mechgrade.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','mechgrade.herokuapp.com']
 
 
 # Application definition
